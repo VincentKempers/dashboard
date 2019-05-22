dcf1103b0f23f04a9db208b4306dc55cbc406d9a
Vincent Kempers <me@vncn.tk>
2019-05-23 00:01:24 +0200

    wip: add todo logic

---
+++ src/lib/todo.js
@@ -0,0 +1,45 @@
+var inputType = document.querySelector('.toDo__input');
+var ul = document.querySelector('#todolist');
+var clearButton = document.querySelector('.clear');
+var allChildren = document.querySelector('#todolist').children;
+var elementsArray;
+var toDoArray;
+
+
+inputType.addEventListener('keypress', function (event) {
+  if (inputType.value === '') {
+    return
+  };
+
+  if (event.key === "Enter") {
+    ul.insertAdjacentHTML('beforeend', `<li>${inputType.value}</li>`);
+    setLocalStorage();
+    return inputType.value = '';
+  };
+});
+
+function setLocalStorage() {
+  elementsArray = Array.from(allChildren);
+  toDoArray = elementsArray.map(function (element) {
+    return element.innerHTML;
+  })
+  console.log('yatta', toDoArray);
+  localStorage.setItem('toDo', toDoArray);
+}
+
+function getLocalStorage() {
+  var storedString = localStorage.getItem('toDo').split(",");
+  console.log('array', storedString);
+  storedString.forEach(function (element) {
+    console.log('hmmmm', element);
+    ul.insertAdjacentHTML('beforeend', `<li>${element}</li>`)
+  })
+}
+
+clearButton.addEventListener('click', function () {
+  ul.innerHTML = '';
+  localStorage.removeItem('toDo');
+}, true);
+
+
+getLocalStorage();
