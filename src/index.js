fed6c485d54a50840f0145b7d7a9f024a9563797
Vincent Kempers <me@vncn.tk>
2020-03-10 18:52:37 +0100

    add all this shizz

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './scss/styles.scss';
 // import UserList class
 import { UserList } from './lib/user-list';
 import { toDo } from './lib/todo';
+import { backGroundAnimation } from './lib/backgroundAnimations';
 
 // export default UserList class
 // I used `defaultExport` to state that variable name doesn't matter
---
+++ src/lib/backgroundAnimations.js
@@ -0,0 +1,9 @@
+var body = document.querySelector('body');
+var backgroundSections = document.querySelectorAll('section');
+
+
+document.addEventListener('scroll', (event) => {
+  console.log(window.scrollY);
+}, true)
+
+
